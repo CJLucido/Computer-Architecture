b3bd380ed77b62b705c9e0ce00551c3529bff900
Carlo <c.jose.lucido@gmail.com>
2020-08-05 18:33:08 -0400

    HLT not working because skipping over 5 with self.pc

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -11,19 +11,32 @@ class Branch_Table:
     def __init__(self):
         # Set up the branch table
         self.branchtable = {}
+        print("LDI",LDI)
         self.branchtable[LDI] = self.handle_ldi
         self.branchtable[PRN] = self.handle_prn
 
-    def handle_ldi(self, register, value):
-        register = value
+    def handle_ldi(self, cpu, register, value):
+        print(cpu)
+        cpu.reg_write(value, int(register,2))
 
-    def handle_prn(self, register):
-        print(int(register, 10))
+    def handle_prn(self, cpu, register):
+        # print(int(register, 10))
+        cpu.ram_read(int(register,2))
 
-    def run(self, ir):
+    def run(self, ir, cpu):
         # Example calls into the branch table
         # ir = LDI
-        self.branchtable[ir]
+        self.branchtable[int(ir, 2)](cpu)
+
+    def run2(self, ir, cpu, a =None):
+        # Example calls into the branch table
+        # ir = LDI
+        self.branchtable[int(ir, 2)](cpu, a)
+
+    def run3(self, ir, cpu, a =None, b= None):
+        # Example calls into the branch table
+        # ir = LDI
+        self.branchtable[int(ir, 2)](cpu, a, b)
 
         # ir = PRN
         # self.branchtable[ir](operand_a, operand_b)
@@ -34,7 +47,7 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         self.ram = [0] * 255
-        self.reg = [0] * 8
+        self.reg = [None] * 8
         self.pc = 0
         #self.reg[5] = interrupt mask (IM)
         #self.reg[6] = interrupt status (IS)
@@ -46,6 +59,9 @@ class CPU:
     def ram_write(self, MDR, MAR):
         # self.reg[self.ram[MAR]] = MDR
         self.ram[MAR] = MDR
+
+    def reg_write(self, MDR, MAR):
+        self.reg[MAR] = MDR
 
     def load(self):
         """Load a program into memory."""
@@ -102,29 +118,37 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        IR = self.ram[self.pc]
+
         pc = self.pc
 
-
-        num_operands = str(IR)[2:4]
+        MAR = 0
+        if MAR > 7:
+            MAR = 0
 
         bt = Branch_Table()
-
+        print("6",self.ram[5])
         while self.ram_read(self.pc) != HLT:
+            IR = self.ram[self.pc]
+            if len(str(IR)) < 8:
+                num_operands = "00" + str(IR)[2]
+            elif len(str(IR)) < 10:
+                num_operands = "0" + str(IR)[2]
+            else:
+                num_operands = str(IR)[2:4]
+            print(num_operands)
+
             if num_operands == "00":
-                bt.run(self.ram[self.pc]) #I need a dictionary to return the function to do based on the read!!
+                bt.run(self.ram[self.pc], self)
             elif num_operands == "01":
-                # operand_a = self.reg[pc + 1] #REG NOT GIVEN ANYTHING YET!!!
-                operand_a = pc + 1#self.ram[pc + 1] 
-                bt.run(self.ram[self.pc])(self.ram[operand_a]) #THIS ISN"T A REGISTER IT IS A VALUE
+                operand_a = pc + 1
+                bt.run2(self.ram[self.pc], self, self.ram[operand_a]) #A VALUE AS A REGISTER
                 self.pc += 1
             elif num_operands == "10":
-                # operand_a =self.reg[pc + 1]
-                # operand_b =self.reg[pc + 2]
                 operand_a =pc + 1
                 operand_b =pc + 2
-                bt.run(self.ram[self.pc])(self.ram[operand_a], self.ram[operand_b]) #SAME
+                bt.run3(self.ram[self.pc], self, self.ram[operand_a], self.ram[operand_b]) #SAME
                 self.pc += 2
             
             self.pc += 1
-        print("halt")
+            print("should", self.pc)
+            print("equal",HLT)
